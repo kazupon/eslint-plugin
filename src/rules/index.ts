ed4fad03f6a1a600d3ccacdc30341723cee01ffd
kazuya kawaguchi <kawakazu80@gmail.com>
2025-12-26 13:04:45 +0900

    fix: set defaultOptions for rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -4,11 +4,24 @@
  */
 
 import enforceHeaderComment from './enforce-header-comment.ts'
-import noTagComments from './no-tag-comments.ts'
+import noTagComments, { DEFAULT_TAGS as NO_TAG_COMMENTS_TAGS } from './no-tag-comments.ts'
 import preferInlineCodeWordsComments from './prefer-inline-code-words-comments.ts'
-import preferScopeOnTagComment from './prefer-scope-on-tag-comment.ts'
+import preferScopeOnTagComment, {
+  DEFAULT_TAGS as PREFER_SCOPE_ON_TAG_COMMENT_TAGS,
+  DEFAULT_DIRECTIVES as PREFER_SCOPE_ON_TAG_COMMENT_DIRECTIVES
+} from './prefer-scope-on-tag-comment.ts'
 
 import type { RuleModule } from '../utils/types.ts'
+
+export const defaultOptions = {
+  'no-tag-comments': {
+    tags: NO_TAG_COMMENTS_TAGS
+  },
+  'prefer-scope-on-tag-comment': {
+    tags: PREFER_SCOPE_ON_TAG_COMMENT_TAGS,
+    directives: PREFER_SCOPE_ON_TAG_COMMENT_DIRECTIVES
+  }
+}
 
 export const rules: Record<string, RuleModule> = {
   'enforce-header-comment': enforceHeaderComment,
